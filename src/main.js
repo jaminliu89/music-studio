65239a8cf8bebc2f630166dfe4bc244121847946
jaminliu89 <jaminliu89@outlook.com>
2026-09-02 03:33:17 +0800

    feat: promote runtime-connected DME Studio to default surface

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import "./app.css";
 import { mount } from "svelte";
 import ToneLabApp from "./App.svelte";
-import DMEApp from "./DMEApp.svelte";
+import DMEStudio from "./DMEStudio.svelte";
 
 const params = new URLSearchParams(window.location.search);
 const legacy = params.get("legacy") === "1";
-const Surface = legacy ? ToneLabApp : DMEApp;
+const Surface = legacy ? ToneLabApp : DMEStudio;
 
 const app = mount(Surface, { target: document.body });
 
